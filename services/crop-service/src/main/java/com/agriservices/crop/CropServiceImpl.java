82a3c7ed8600ead3fba52605a175c3755dd48ac5
wanes2A <wane96208@gmail.com>
2025-12-17 15:19:33 +0100

    Implement Crop management methods in CropServiceImpl

--- services/crop-service/src/main/java/com/agriservices/crop/CropServiceImpl.java
+++ services/crop-service/src/main/java/com/agriservices/crop/CropServiceImpl.java
@@ -1,12 +1,31 @@
 package com.agriservices.crop;
-
 import jakarta.jws.WebService;
+import model.Crop;
+import java.util.*;
 
 @WebService(endpointInterface = "com.agriservices.crop.CropService")
-public class CropServiceImpl implements CropService {
-    
+private final Map<String, Crop> crops = new HashMap<>();
+
     @Override
-    public String hello() {
-        return "Hello World from Crop Service (SOAP)!";
+    public List<Crop> getAllCrops() {
+        return new ArrayList<>(crops.values());
+    }
+
+    @Override
+    public Crop getCropById(String id) {
+        return crops.get(id);
+    }
+
+    @Override
+    public Crop createCrop(Crop crop) {
+        String id = UUID.randomUUID().toString();
+        crop.setId(id);
+        crops.put(id, crop);
+        return crop;
+    }
+
+    @Override
+    public boolean deleteCrop(String id) {
+        return crops.remove(id) != null;
     }
 }

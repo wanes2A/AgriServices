af4fa32d8d25337326945bd2d82cd2174e3e6a42
wanes2A <wane96208@gmail.com>
2025-12-17 15:27:39 +0100

    Refactor SERVICE_URL declaration and logging messages

--- services/crop-service/src/main/java/MainPublisher.java
+++ services/crop-service/src/main/java/MainPublisher.java
@@ -1,18 +1,18 @@
 // File: services/crop-service/src/main/java/MainPublisher.java
 
 import javax.xml.ws.Endpoint;
+import service.CropServiceImpl;
 
 public class MainPublisher {
 
-    // Le port configuré pour ce service
-    private static final String SERVICE_URL = "http://localhost:8082/CropService";
+    private static final String SERVICE_URL =
+            "http://localhost:8082/CropService";
 
     public static void main(String[] args) {
         System.out.println("Démarrage du service SOAP Crop-Service sur: " + SERVICE_URL);
 
-        // Publie l'implémentation du service à l'URL spécifiée
         Endpoint.publish(SERVICE_URL, new CropServiceImpl());
 
-        System.out.println("Service prêt. Lien WSDL (documentation) : " + SERVICE_URL + "?wsdl");
+        System.out.println("Service prêt. WSDL : " + SERVICE_URL + "?wsdl");
     }
 }
